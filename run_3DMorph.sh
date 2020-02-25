9bd97bc1820b9ffa57df722f634c6aacc4c56304
NiCl2 <nicholas.clifton@outlook.com>
2020-02-25 09:37:12 +0000

    update array number

--- run_3DMorph.sh
+++ run_3DMorph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=0-24:00
-#SBATCH --array=0-100
+#SBATCH --array=0-6509
 #SBATCH --partition=c_compute_neuro1_long
 #SBATCH --job-name=Morph3D
 #SBATCH --account=scw1354
@@ -10,8 +10,14 @@
 #SBATCH --output=OUT/%A_%a-%J.o
 #SBATCH --error=ERR/%A_%a-%J.e
 
+echo "**** Job starts ****"
+date
+
 module load matlab
 
 data_list=( /scratch/c.c0808508/morph3D/input/* )
 
 matlab -nosplash -nodesktop -nodisplay -r "morph3D('input/${data_list[$SLURM_ARRAY_TASK_ID]}');"
+
+echo "**** Job ends ****"
+date
