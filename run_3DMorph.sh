95e87db0d1433ed97f469369fe6003f2ca48504d
NiCl2 <nicholas.clifton@outlook.com>
2020-02-26 10:42:22 +0000

    adjusted for NEC

--- run_3DMorph.sh
+++ run_3DMorph.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #SBATCH --mem-per-cpu=4G
+#SBATCH --time=0-24:00
 #SBATCH --array=0
 #SBATCH --partition=c_compute_neuro1
-#SBATCH --account=scw1349
+#SBATCH --job-name=Morph3D
+#SBATCH --account=scw1354
+#SBATCH --mail-type=END
+#SBATCH --mail-user=cliftonne@cardiff.ac.uk
 #SBATCH --output=OUT/%A_%a-%J.o
 #SBATCH --error=ERR/%A_%a-%J.e
 
@@ -11,7 +15,9 @@ date
 
 module load matlab
 
-data_list=( input/* )
+data_list=(/scratch/c.c0808508/Morph3D/input/*)
+
+echo "${data_list[$SLURM_ARRAY_TASK_ID]}"
 
 matlab -nosplash -nodesktop -nodisplay -r "morph3D('input/${data_list[$SLURM_ARRAY_TASK_ID]}');"
 
